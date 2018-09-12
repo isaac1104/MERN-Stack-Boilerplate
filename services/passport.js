433636f74d7211210cf8ef468790a146b44c40bc
Isaac Kwon <isaac-kwon@hotmail.com>
2018-09-12 14:26:32 -0700

    add local strategy and compare pw

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,21 @@ const User = require('../models/user');
 const keys = require('../config/keys');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+const localLogin = new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+
+    if (!user) {
+      return done(null, false);
+    }
+
+
+  })
+});
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
