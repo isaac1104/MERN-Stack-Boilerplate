447112bc24957afcb8aa95abf41a5270e5b35624
Isaac Kwon <isaac-kwon@hotmail.com>
2018-09-12 14:21:07 -0700

    authenticate user using passport

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,4 +1,8 @@
 const Authentication = require('../controllers/authentication');
+const passportService = require('../services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = app => {
   app.post('/api/signup', Authentication.signup);
---
+++ services/passport.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const User = require('../models/user');
+const keys = require('../config/keys');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: keys.secret
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  User.findById(payload.sub, (err, user) => {
+    if (err) {
+      return done(err, false);
+    }
+
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);
